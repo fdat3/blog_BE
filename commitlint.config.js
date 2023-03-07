2c9e9255494b5b554f1138f9d59b58bb485c4df3
Le Thanh Nhan <nhanlethanh1198@gmail.com>
2023-03-07 12:06:26 +0700

    sequelize: fix init model

--- commitlint.config.js
+++ commitlint.config.js
@@ -45,7 +45,8 @@ module.exports = {
                 "translation",
                 "security",
                 "changeset",
-                "swagger"
+                "swagger",
+                "sequelize"
             ]
         ],
     },
