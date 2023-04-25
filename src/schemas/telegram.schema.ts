22a851cbef98d1c69c8f8a3f07cd735f5c12dd11
fdat3 <datp0628@gmail.com>
2023-04-25 22:58:52 +0700

    init: update

--- src/schemas/telegram.schema.ts
+++ src/schemas/telegram.schema.ts
@@ -1,15 +1,15 @@
-import mongoose from 'mongoose'
+// import mongoose from 'mongoose'
 
-const TelegramSchema = new mongoose.Schema(
-  {
-    message_thread_id: {
-      type: String,
-      unique: true,
-    },
-  },
-  {
-    versionKey: false,
-    timestamps: true,
-  },
-)
-export default TelegramSchema
+// const TelegramSchema = new mongoose.Schema(
+//   {
+//     message_thread_id: {
+//       type: String,
+//       unique: true,
+//     },
+//   },
+//   {
+//     versionKey: false,
+//     timestamps: true,
+//   },
+// )
+// export default TelegramSchema
