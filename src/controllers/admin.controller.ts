4d4927405b8bf5ad047baa9a602c6796dd09f591
fdat3 <datp0628@gmail.com>
2023-04-27 01:49:24 +0700

    fix: error undefined variable

--- src/controllers/admin.controller.ts
+++ src/controllers/admin.controller.ts
@@ -1,30 +1,30 @@
-import ConstantAPI from '@/constants/api.constant'
-import adminUserController from '@/controllers/admin/admin_user.controller'
-import Controller from '@/interfaces/controller.interface'
-import Authenticated from '@/middlewares/authenticated.middleware'
-import { verifyToken } from '@/validations/token.validation'
-import { Router } from 'express'
+// import ConstantAPI from '@/constants/api.constant'
+// import adminUserController from '@/controllers/admin/admin_user.controller'
+// import Controller from '@/interfaces/controller.interface'
+// import Authenticated from '@/middlewares/authenticated.middleware'
+// import { verifyToken } from '@/validations/token.validation'
+// import { Router } from 'express'
 
-class AdminController implements Controller {
-  public router: Router
-  public path: string
+// class AdminController implements Controller {
+//   public router: Router
+//   public path: string
 
-  private authenticated: Authenticated
+//   private authenticated: Authenticated
 
-  constructor() {
-    this.router = Router()
-    this.path = ConstantAPI.ADMIN
-    this.authenticated = new Authenticated()
-    this.initialiseRoutes()
-  }
+//   constructor() {
+//     this.router = Router()
+//     this.path = ConstantAPI.ADMIN
+//     this.authenticated = new Authenticated()
+//     this.initialiseRoutes()
+//   }
 
-  private initialiseRoutes(): void {
-    this.router.use(
-      `${this.path}${ConstantAPI.ADMIN_USER}`,
-      [verifyToken, this.authenticated.verifyTokenAndAdmin],
-      adminUserController.router,
-    )
-  }
-}
+//   private initialiseRoutes(): void {
+//     this.router.use(
+//       `${this.path}${ConstantAPI.ADMIN_USER}`,
+//       [verifyToken, this.authenticated.verifyTokenAndAdmin],
+//       adminUserController.router,
+//     )
+//   }
+// }
 
-export default AdminController
+// export default AdminController
