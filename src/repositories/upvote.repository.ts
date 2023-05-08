d23b019adbe05b1affd8a52a5d3df80d50be9a19
fdat3 <datp0628@gmail.com>
2023-05-08 22:52:54 +0700

    fix: clean code

--- src/repositories/upvote.repository.ts
+++ src/repositories/upvote.repository.ts
@@ -1,5 +1,5 @@
 import { sequelize } from '@/config/sql.config'
-import { UpVote } from '@/models/pg'
+import { Blog, UpVote } from '@/models/pg'
 import logger from '@/utils/logger.util'
 // import { ICrudOption } from '@/interfaces/controller.interface'
 // import {
@@ -15,11 +15,46 @@ class UpVoteRepository {
     this.model = UpVote
   }
 
-  public async create(data: any): Promise<Partial<UpVote> | null> {
+  // public async create(data: any): Promise<Partial<UpVote> | any> {
+  //   try {
+  //     const result: UpVote = await sequelize.transaction(
+  //       async (transaction) => {
+  //         UpVote.create(data, { transaction })
+  //         return Blog.increment('upVote', { by: 1, where: { id: data.blogId } })
+  //       },
+  //     )
+  //     return result.get({ plain: true })
+  //   } catch (error) {
+  //     logger.error(error)
+  //     return null
+  //   }
+  // }
+
+  public async create(data: any): Promise<UpVote | any> {
     try {
       const result: UpVote = await sequelize.transaction(
         async (transaction) => {
-          return this.model.create(data, { transaction })
+          const upVote = await UpVote.create(data, { transaction })
+          if (upVote) {
+            await Blog.findOne({
+              where: {
+                id: data.blogId,
+              },
+              transaction,
+            }).then((instance) => {
+              if (instance) {
+                instance?.increment(
+                  {
+                    upVote: 1,
+                  },
+                  {
+                    transaction,
+                  },
+                )
+              } else return
+            })
+          }
+          return upVote
         },
       )
       return result.get({ plain: true })
