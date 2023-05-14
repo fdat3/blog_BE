2462e4c96d40e118b4f6d9f9c879894d7f0039ea
fdat3 <datp0628@gmail.com>
2023-05-14 23:40:16 +0700

    update: delete poll

--- src/types/poll.type.ts
+++
@@ -1,6 +0,0 @@
-import { EPollVoteActionType } from '@/enums/pollVote.enum'
-import { PollVoteInterface } from '@/interfaces/poll.interface'
-
-export type TPollVoteActionType = keyof typeof EPollVoteActionType
-
-export type TPollVoteRepository = Omit<PollVoteInterface, 'action'>
